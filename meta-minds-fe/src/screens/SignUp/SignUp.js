4c5dac4959794f1eb7753c5a280d96004e044456
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2021-11-22 17:33:25 -0500

    styled the sign up page

--- meta-minds-fe/src/screens/SignUp/SignUp.js
+++ meta-minds-fe/src/screens/SignUp/SignUp.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { Link } from "react-router-dom";
+import "./SignUp.css";
 
 export default function SignUp(props) {
   const [formData, setFormData] = useState({
@@ -18,38 +20,57 @@ export default function SignUp(props) {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleRegister(formData);
-      }}
-    >
-      <label>
-        Username:
-        <input
-          type='text'
-          name='username'
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Email:
-        <input type='text' name='email' value={email} onChange={handleChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type='password'
-          name='password'
-          value={password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <button>Submit</button>
-    </form>
+    <div class="sign-up-div">
+      <div class="meta-minds-logo-div">
+        <img className="meta-minds-logo" src="https://res.cloudinary.com/tylerwashington98/image/upload/v1636143053/Meta-Minds/2d4b6fe46ee740998e2e0f51bbbd3496_esrod4.png" alt="Meta Minds Logo"></img>
+        <h2 className='copyright-text'>copyright 2021</h2>
+      </div >
+      <form class='sign-up-form'
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleRegister(formData);
+        }}
+      >
+        <div class='sign-in-header-text'>Sign-In</div>
+        <label class='sign-in-label-and-input-div'>
+          <div class='input-text-title'>Username</div>
+          <input
+            class='user-input-box'
+            type='text'
+            name='username'
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label class='sign-in-label-and-input'>
+          <div class='input-text-title'>Email</div>
+          <input
+            class='user-input-box'
+            type='text'
+            name='email'
+            value={email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label class='sign-in-label-and-input' >
+          <div class='input-text-title'>Password</div>
+          <input
+            class='user-input-box'
+            type='password'
+            name='password'
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <Link to='/explore'><button class='sign-up-button'>Sign-Up</button></Link>
+      </form>
+      <div class='sign-in-div'>
+        <div class='have-account-text'>Have an Account?</div>
+        <Link to='/sign-in'><button class='go-to-sign-in-button'>Sign-In</button></Link>
+      </div>
+    </div>
   );
 }
