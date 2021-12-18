101596927fa9885817b69398d9c418672af13aac
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2021-12-18 15:28:42 -0500

    set pointer-events on .page-overlay to none so that I can scroll the full page when the search bar dropdown is open

--- meta-minds-fe/src/screens/UserPosts/UserPosts.jsx
+++ meta-minds-fe/src/screens/UserPosts/UserPosts.jsx
@@ -10,28 +10,33 @@ export default function YourPosts(props) {
   const { currentUser, posts } = props
   const [allPosts, setAllPosts] = useState([])
   const [newestPost, setNewestPost] = useState([])
+  const [loading, setLoading] = useState(false)
   const { DateTime } = require("luxon");
 
-  console.log(currentUser)
 
   useEffect(async () => {
-    const userPosts = await posts.filter((post) => {
-      return post.user_id === currentUser.id
+    setLoading(true);
+    const userPosts = posts.filter((post) => {
+      return post.user_id == currentUser.id
     })
     const mostRecentPost = await userPosts.sort((b, a) => {
       return a.id - b.id
     })
     setNewestPost(mostRecentPost[0])
     setAllPosts(userPosts)
+    setLoading(false);
   }, [currentUser])
+
+  console.log(newestPost)
 
   return (
     <div>
-      <img className="user-post--banner-image" src={newestPost?.image} alt={newestPost?.title}></img>
+      <img className="user-post--banner-image" src="https://res.cloudinary.com/tylerwashington98/image/upload/v1638051076/Meta-Minds/decentraland_naqec7.jpg" alt="Create-Post-Banner-Image"></img>
       <div className="user-post-and-button-div">
         <div className="create-post-button-div">
           <button className="create-post-button"><Link to='/create-post' className="create-post-button-link" >Create Post</Link></button>
         </div>
+        {newestPost === undefined && loading === false ? (<h2 className='firstpost'>Start influencing the creation of Web3 by making your first post!</h2>) : (newestPost === [] && loading === true ? (<div>Loading...</div>) : (<div></div>))}
         <div class="user-post-outer-div">
           {allPosts.map(post => {
             return (
