47e3a10d887f3e9fd88fd96e5d15f95a0bb3a308
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2022-01-21 15:17:26 -0500

    added heroku server

--- meta-minds-fe/src/j/Api_config.js
+++ meta-minds-fe/src/j/Api_config.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 const baseUrl =
-  process.env.NODE_ENV === 'production'
-    ?
-    'ChangeLater' :
-    'http://localhost:3000';
+  process.env.NODE_ENV === 'production' ? 'https://secret-ravine-54617.herokuapp.com/' : 'http://localhost:3000';
 
 const api = axios.create({
   baseURL: baseUrl
