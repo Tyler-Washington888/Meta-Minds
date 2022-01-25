486ec0cb710f18d1677b96f13c1a4073c19ed6f9
Tyler Washington <tylerwashington@tylers-air.hamilton.edu>
2022-01-25 13:10:00 -0500

    changed hamburger menu

--- meta-minds-fe/src/components/Header/Header.jsx
+++ meta-minds-fe/src/components/Header/Header.jsx
@@ -181,19 +181,19 @@ function Header(props) {
                 <h3>Categories</h3>
                 <div className='category-links'>
                   <NavLink exact to="/" onClick={() => setOpenHamburger(!openHamburger)} activeStyle={{
-                    color: "#0BC9E3"
+                    border: '1px solid '
                   }} className="nav-Links"><div className='links'>Latest</div></NavLink>
                   <NavLink to="/meta" onClick={() => setOpenHamburger(!openHamburger)} activeStyle={{
-                    color: "#0BC9E3"
+                    border: '1px solid '
                   }} className="nav-Links"><div className='links'>Meta</div></NavLink>
                   <NavLink to="/mana" onClick={() => setOpenHamburger(!openHamburger)} activeStyle={{
-                    color: "#0BC9E3"
+                    border: '1px solid '
                   }} className="nav-Links"><div className='links'>Mana</div></NavLink>
                   <NavLink to="/crypto" onClick={() => setOpenHamburger(!openHamburger)} activeStyle={{
-                    color: "#0BC9E3"
+                    border: '1px solid '
                   }} className="nav-Links"><div className='links'>Crypto</div></NavLink>
                   <NavLink to="/film" onClick={() => setOpenHamburger(!openHamburger)} activeStyle={{
-                    color: "#0BC9E3"
+                    border: '1px solid '
                   }} className="nav-Links"><div className='links'>Film</div></NavLink>
                 </div>
               </div>
