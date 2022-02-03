b112b73c24c276dab82df2b916f9c9ec63c87882
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2022-02-02 23:04:27 -0500

    chnaged location of loading icon

--- meta-minds-fe/src/screens/Explore/Explore.jsx
+++ meta-minds-fe/src/screens/Explore/Explore.jsx
@@ -137,7 +137,7 @@ function Explore(props) {
       {done ? (
         <div></div>
       ) : (<div className='loadingContentScreen'>
-        <ReactLoading type="spokes" color="white" height={100} width={50} />
+        <ReactLoading type="spokes" color="white" className="loadicon" height={100} width={50} />
       </div>)}
     </div >
   )
